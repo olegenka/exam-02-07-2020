d4d1a32741932ca48a595aa9c3547bcd4b85932f
kill Bill <60369904+olegenka@users.noreply.github.com>
2020-07-02 11:47:39 +0300

    time

---
+++ exam-02-07-2020/scr/exam-02-07-2020.cpp
@@ -0,0 +1,51 @@
+﻿
+
+#include <iostream>
+using namespace std;
+#pragma once
+
+struct arrow
+{
+	
+	int number;
+	int first;
+	int second;
+};
+class blok
+{
+	
+public:
+	int nom;
+	int to;
+	int i;
+	int x;
+	arrow *mas= new mas[x];
+	void add(int nom, int to)
+	{
+		arrow struckt1; 
+		struckt1.number = i;
+		struckt1.first = nom;
+		struckt1.second = to;
+	}
+	void del(int nom, int to)
+	{
+		for (int j = 0; j < i; i++)
+		{
+			if (mas[j].first == nom and mas[j].second == to)
+			{
+			
+				for (int k = j + 1; k < i; k++)
+					mas[k - 1] = mas[k];
+			}
+		
+		}
+	
+	
+	}
+};
+
+int main()
+{
+    
+}
+
